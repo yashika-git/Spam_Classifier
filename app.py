e062d45ea3c8ad358573af4f2ff3b64b5622b99a
yashikajain2001 <40869641+yashikajain2001@users.noreply.github.com>
2021-11-03 12:09:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ if st.button("Predict"):
     vector_input = tfidf.transform([transformed_text])
     result = model.predict(vector_input)[0]
 
-if result==1:
-    st.header("Spam")
-else:
-    st.header("Not Spam")
+    if result==1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
 
 
 # In[ ]:
